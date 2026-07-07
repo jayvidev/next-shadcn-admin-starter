8e7545fb3eb746feea2db9cb29c2397063d097e2
jayvidev <jason.vilac@gmail.com>
2026-07-07 17:37:25 -0500

    fix(sidebar): add class to Logo component for consistent styling

--- features/admin/components/sidebar/app-sidebar.tsx
+++ features/admin/components/sidebar/app-sidebar.tsx
@@ -29,7 +29,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <Link href="/admin">
                 <div className="flex aspect-square size-8 items-center justify-center">
-                  <Logo />
+                  <Logo className="size-8! shrink-0" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-semibold">Admin Starter</span>
